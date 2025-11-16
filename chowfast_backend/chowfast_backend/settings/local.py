77a1c864f974dc57a4e0f0a257b69d044807d23b
josesholly <josepholawuyi30@gmail.com>
2025-11-16 17:52:44 +0100

    remove: STATICFILES_DIRS from all settings

--- chowfast_backend/chowfast_backend/settings/local.py
+++ chowfast_backend/chowfast_backend/settings/local.py
@@ -38,9 +38,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
